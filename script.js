57bae79778ec42eddfee2c4ff01b4e45ac56d649
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-16 01:29:02 +0200

    opdtacht-if-else js

---
+++ script.js
@@ -0,0 +1,26 @@
+const age = 18;
+if (age >= 18) {
+console.log("allowed");
+}
+
+ else if (age < 18) {
+console.log("not allowed");
+}
+
+
+const isFemale = "true";
+if (isFemale) {
+    console.log("true");
+}
+else {
+console.log("false");
+
+}
+
+const driverStatus = 'Bob';
+if (driverStatus) {
+console.log("can drive") ;
+}
+else {
+    console.log("can't drive");
+}
