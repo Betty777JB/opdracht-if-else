8272ce7fa4c725f95fdbd1c6375f5f097f9247ad
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-18 16:14:02 +0200

    kleine wijzigingen

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log("not allowed");
 }
 
 
-const isFemale = "true";
+const isFemale = true; //!!!!!true zonder aanhalingsteken
 if (isFemale) {
     console.log("true");
 }
@@ -24,3 +24,25 @@ console.log("can drive") ;
 else {
     console.log("can't drive");
 }
+
+
+/*  oplossing
+if (age >= 18) {
+  console.log("Je mag naar binnen");
+} else {
+  console.log("Nope, je mag niet naar binnen");
+}
+
+if (isFemale) {
+  console.log("Je ticket zegt dat je een vrouw bent!");
+} else {
+  console.log(
+    "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+  );
+}
+
+if (driverStatus === "bob") {
+  console.log("Je bent de bob, je mag autorijden");
+} else {
+  console.log("Je bent geen bob, hier met die autosleutels");
+}*/
